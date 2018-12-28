b44e11f7e0ab26abcc2c8c8810141b18c06e3105
i2xzy <yitzchokr@live.com>
2018-12-28 17:13:30 +0000

    write function that will get video details from the youtube api

---
+++ src/api.js
@@ -0,0 +1,24 @@
+import { client } from 'gapi-client';
+
+export default () => {
+  client.init({ apiKey: process.env.REACT_APP_API_KEY });
+  return client
+    .load('youtube', 'v3')
+    .then(() =>
+      client.youtube.channels.list({
+        id: 'UCvO6uJUVJQ6SrATfsWR5_aA',
+        part: 'snippet,contentDetails'
+      })
+    ) // 1) request a list of channels that match the channel id with details about uploads
+    .then(({ result }) =>
+      client.youtube.playlistItems.list({
+        playlistId: result.items[0].contentDetails.relatedPlaylists.uploads,
+        part: 'snippet,contentDetails',
+        maxResults: '49'
+      })
+    ) // 2) request playlist of videos that match the uploads of our channel
+    .then(({ result }) => result.items) // 3) return just the array of videos
+    .catch(err => {
+      throw new Error(`api fetch failed ${err}`);
+    });
+};
